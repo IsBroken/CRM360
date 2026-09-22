33e347f9e7e5e8b233beae3f8fcd7f6a0c46e569
MohdAyaanShaikh <shaikhayaan1459@gmail.com>
2026-09-22 10:50:11 +0530

    adding correct url in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000'
+export const API_BASE_URL = https://crm360-ogo1.onrender.com
 function getAuthToken() {
   return localStorage.getItem('token')
 }
